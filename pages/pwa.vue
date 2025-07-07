93519efff99310149f725d1eb1ef463c69b567f8
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-07-07 06:19:38 +0300

    feat: add PWA support with offline readiness and content update notification.

---
+++ pages/pwa.vue
@@ -0,0 +1,19 @@
+<script setup>
+// If you want to use it in setup, import from the nuxtApp.
+const { $pwa } = useNuxtApp();
+
+const message = useMessage();
+
+onMounted(() => {
+  if ($pwa.offlineReady) message.success("App ready to work offline");
+});
+</script>
+
+<template>
+  <!-- You can use $pwa directly in templates! -->
+  <div v-show="$pwa.needRefresh">
+    <span> New content available, click on reload button to update. </span>
+
+    <button @click="$pwa.updateServiceWorker()">Reload</button>
+  </div>
+</template>
